e14d166afdb6c2e8f23f6214f79108196e5d4866
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-10-18 17:46:47 +0700

    fix minimax

--- src/MinimaxBot.java
+++ src/MinimaxBot.java
@@ -3,10 +3,13 @@ import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
 import javafx.scene.control.Button;
 
 public class MinimaxBot extends Bot{
+    private int depth = 4;
+    private int roundsLeft;
 
     @Override
     public int[] move(Button[][] position, String playerLabel, int roundsLeft)  {
         // int max = Integer.MIN_VALUE;
+        this.roundsLeft = roundsLeft;
         int[] selectedLoc = {-1, -1};
         Button[][] tempMap = new Button[position.length][position[0].length];
         for(int i = 0 ; i < position.length; i++){
@@ -16,35 +19,45 @@ public class MinimaxBot extends Bot{
                 tempMap[i][j] = copyMap;
             }
         }
-        int[] a = minimax(tempMap, roundsLeft, Integer.MAX_VALUE, Integer.MIN_VALUE, playerLabel);
+        int[] a = minimax(tempMap, depth, Integer.MAX_VALUE, Integer.MIN_VALUE, playerLabel, true);
         selectedLoc[0] = a[1];
         selectedLoc[1] = a[2];
         return selectedLoc;
     }
     
-    public int minimaxEval(Button[][] maploc) {
+    public int minimaxEval(Button[][] maploc, String playerLabel) {
         int eval = 0;
         for(int i = 0 ; i < 8; i++){
             for(int j = 0 ; j < 8; j++){
-                if(maploc[i][j].getText().equals("O")) {
-                    eval++;
-                }
-                if(maploc[i][j].getText().equals("X")) {
-                    eval--;
+                if(playerLabel=="O"){
+                    if(maploc[i][j].getText().equals("O")) {
+                        eval++;
+                    }
+                    if(maploc[i][j].getText().equals("X")) {
+                        eval--;
+                    }
+                }else{
+                    if(maploc[i][j].getText().equals("X")) {
+                        eval++;
+                    }
+                    if(maploc[i][j].getText().equals("O")) {
+                        eval--;
+                    }
+
                 }
             }
         }
         return eval;
     }
 
-    public int[] minimax(Button[][] maploc, int roundsLeft, int alpha, int beta, String playerLabel){
+    public int[] minimax(Button[][] maploc, int depth, int alpha, int beta, String playerLabel, boolean isMaximazing){
 
-        if(roundsLeft == 0) {
-            int evaluation = minimaxEval(maploc);
+        if(depth == 0 || this.roundsLeft==0) {
+            int evaluation = minimaxEval(maploc,playerLabel);
             return new int[] {evaluation, -1, -1};
         }
 
-        if(playerLabel == "O") {
+        if(isMaximazing) {
             int maxEval = Integer.MIN_VALUE;
             int bestX = -1;
             int bestY = -1;
@@ -52,8 +65,8 @@ public class MinimaxBot extends Bot{
             for(int i = 0; i < 8; i++){
                 for(int j = 0 ; j < 8; j++){
                     if(maploc[i][j].getText().equals("")){
-                        Button[][] newPos = updateVirtualPosition(i, j, maploc, true);
-                        int[] selection =  minimax(newPos, roundsLeft - 1, alpha, beta, "X");
+                        Button[][] newPos = updateVirtualPosition(i, j, maploc, playerLabel);
+                        int[] selection =  minimax(newPos, depth - 1, alpha, beta, playerLabel,!isMaximazing);
                         int eval = selection[0];
 
                         if (eval > maxEval){
@@ -63,7 +76,9 @@ public class MinimaxBot extends Bot{
                         }
 
                         alpha = Math.max(alpha, eval);
-                        if(beta < alpha) break;
+                        if(beta <= alpha) {
+                            break;
+                        }
                     }
                 }
             }
@@ -77,8 +92,8 @@ public class MinimaxBot extends Bot{
             for(int i = 0 ; i < 8 ; i++){
                 for(int j = 0 ; j < 8; j++){
                     if (maploc[i][j].getText().equals("")) {
-                        Button[][] newPos = updateVirtualPosition(i, j, maploc,false);
-                        int[] selection = minimax(newPos, roundsLeft - 1, alpha, beta, "O");
+                        Button[][] newPos = updateVirtualPosition(i, j, maploc,playerLabel);
+                        int[] selection = minimax(newPos, depth - 1, alpha, beta, playerLabel, !isMaximazing);
                         int eval = selection[0];
 
                         if(eval < minEval){
@@ -88,6 +103,9 @@ public class MinimaxBot extends Bot{
                         }
 
                         beta = Math.min(beta, eval);
+                        if(beta <= alpha) {
+                            break;
+                        }
                     }
                 }
             }
@@ -95,7 +113,7 @@ public class MinimaxBot extends Bot{
         }
     }
 
-    public Button[][] updateVirtualPosition(int x, int y, Button[][] position, boolean playerO) {
+    public Button[][] updateVirtualPosition(int x, int y, Button[][] position, String playerLabel) {
         Button[][] finalPosition = new Button[position.length][position[0].length];
         for(int i = 0 ; i <position.length;i++){
             for(int j = 0 ; j < position[0].length; j++){
@@ -105,12 +123,12 @@ public class MinimaxBot extends Bot{
             }
         }
 
-        if(playerO){
-            finalPosition[x][y].setText("0");
+        if(playerLabel=="O"){
+            finalPosition[x][y].setText("O");
             if(x > 0 && finalPosition[x-1][y].getText().equals("X")){
                 finalPosition[x-1][y].setText("O");
             }
-            if(x < 7 && finalPosition[x-1][y].getText().equals("X")){
+            if(x < 7 && finalPosition[x+1][y].getText().equals("X")){
                 finalPosition[x+1][y].setText("O");
             }
             if(y > 0 && finalPosition[x][y-1].getText().equals("X")){
@@ -125,7 +143,7 @@ public class MinimaxBot extends Bot{
             if(x > 0 && finalPosition[x-1][y].getText().equals("O")){
                 finalPosition[x-1][y].setText("X");
             }
-            if(x < 7 && finalPosition[x-1][y].getText().equals("O")){
+            if(x < 7 && finalPosition[x+1][y].getText().equals("O")){
                 finalPosition[x+1][y].setText("X");
             }
             if(y > 0 && finalPosition[x][y-1].getText().equals("O")){
